b237579d40128cdc62e7cad164b538dd269e2401
Chiku <chirantan.mitra@gmail.com>
2011-12-02 20:34:23 +0530

    Minimal styling

--- public/javascripts/patients.js
+++ public/javascripts/patients.js
@@ -64,11 +64,18 @@ Application.Views.Patients.Index = Backbone.View.extend({
 
   render: function(patients) {
     _(patients).each(function(patient) {
-      jQuery('<div>', {
-        'class': 'patient',
-        'text': patient.get('name')
-      }).appendTo(this.patients);
+      this.createNodeFor(patient);
     }, this);
+  },
+
+  createNodeFor: function(patient) {
+    var patientNameElement = jQuery('<div>', { 'class': 'patient-name', 'text': patient.get('name') });
+    var doctorNameElement = jQuery('<div>', { 'class': 'doctor-name', 'text': '(' + patient.get('doctor_name') + ')'});
+    var patientIdElement = jQuery('<div>', { 'class': 'patient-id', 'text': patient.get('id') });
+    var patientElement = jQuery('<div>', { 'class': 'patient'});
+    var clearElement = jQuery('<div>', { 'class': 'clear-left'})
+    patientElement.append(patientNameElement).append(doctorNameElement).append(patientIdElement).append(clearElement);
+    patientElement.appendTo(this.patients);
   }
 });
 
