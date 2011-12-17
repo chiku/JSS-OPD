b8d8ac84b99b0a529ec4c6e5612e3a85cc6503ea
Chiku <chirantan.mitra@gmail.com>
2011-12-18 00:44:53 +0530

    Include special index file that serves out minified files and prefers content from CDNs

---
+++ public/javascripts/all.js
@@ -0,0 +1,14 @@
+/**
+ * @depends boot/initialize.js
+
+ * @depends config/routes.js
+ * @depends config/selectors.js
+ * @depends config/urls.js
+
+ * @depends app/models/patient.js
+ * @depends app/collections/patients.collection.js
+ * @depends app/routers/patients.router.js
+ * @depends app/views/patients.view.index.js
+
+ * @depends boot/run.js
+ */
