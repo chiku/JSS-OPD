98c4047ce2370e7b0020da6a79ebc5266ca68796
Chiku <chirantan.mitra@gmail.com>
2011-12-17 21:43:42 +0530

    Move out HTML template code from javascript file into HTML

--- public/javascripts/app/views/patients.view.index.js
+++ public/javascripts/app/views/patients.view.index.js
@@ -2,7 +2,12 @@ Application.Views.Patients = Application.Views.Patients || {};
 
 Application.Views.Patients.Index = Backbone.View.extend({
   tagName: 'section',
+
   className: 'patient',
+
+  template: function() {
+    return _.template(jQuery("#template-patient").html())
+  },
 
   initialize: function(options) {
     this.collection.bind('reset', this.render, this);
@@ -17,18 +22,8 @@ Application.Views.Patients.Index = Backbone.View.extend({
   },
 
   createNodeFor: function(patient) {
-    var template = _.template(
-         "<div class='patient-name'> <%= patientName %> </div>" +
-         "<div class='patient-more-info'>" +
-           "<div class='doctor-name'> <%= doctorName %> </div>" +
-           "<div class='patient-id'> <%= patientId %> </div>" +
-         "</div>");
-
-    jQuery(template({
-      patientName: patient.get('name'),
-      doctorName: patient.get('doctor_name'),
-      patientId: patient.get('id')
-    })).appendTo(this.el);
+    var patientHtml = this.template()(patient.toJSON());
+    jQuery(patientHtml).appendTo(this.el);
   },
 
   attachContentToCleanContainer: function() {
