6892226812b95af811253a7de00b19bbdcce8da7
Chiku <chirantan.mitra@gmail.com>
2012-01-06 23:24:33 +0530

    New minified files

--- public/javascripts/all.js
+++ public/javascripts/all.js
@@ -4,13 +4,14 @@
  * @depends config/selectors.js
  * @depends config/urls.js
 
+ * @depends app/models/encounter.js
  * @depends app/models/patient.js
- * @depends app/collections/patients.collection.js
- * @depends app/routers/patients.router.js
- * @depends app/views/patients.index.js
- * @depends app/views/patient.info.js
+ * @depends app/collections/encounters.collection.js
+ * @depends app/router.js
+ * @depends app/views/encounters.index.js
+ * @depends app/views/encounter.show.js
  * @depends app/views/patient.show.js
- * @depends app/views/patients.sort.js
+ * @depends app/views/encounters.sort.js
 
  * @depends boot/run.js
  */
