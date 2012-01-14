f0372f67a783426f5ba85e7c0c206f503b6fa57d
Chiku <chirantan.mitra@gmail.com>
2012-01-14 17:08:46 +0530

    Rename javascript files to shorter names

--- public/javascripts/all.js
+++ public/javascripts/all.js
@@ -6,11 +6,11 @@
 
  * @depends app/models/encounter.js
  * @depends app/models/patient.js
- * @depends app/collections/encounters.collection.js
+ * @depends app/collections/encounters.js
  * @depends app/router.js
- * @depends app/views/encounters.index.js
- * @depends app/views/encounter.show.js
- * @depends app/views/patient.show.js
+ * @depends app/views/encounters.js
+ * @depends app/views/encounter.js
+ * @depends app/views/patient.js
  * @depends app/views/encounters.sort.js
 
  * @depends boot/run.js
